c7c9ad4902cf0a532a2c0f15cbfd3c08b5041acc
Clement Escoffier <clement.escoffier@gmail.com>
2020-04-12 10:12:33 +0200

    Rework Github Action pipeline
    * separate the build of the different images
    * to not squash the tooling image as the agent does not have enough memory

---
+++ .github/build-s2i-binary-images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+IMAGE_VERSION=$(cat image.yaml | egrep ^version  | cut -d"\"" -f2)
+BUILD_ENGINE=docker
+NAME=quay.io/quarkus/ubi-quarkus-native-binary-s2i:${IMAGE_VERSION}
+
+virtualenv --python=python3 ~/cekit
+source ~/cekit/bin/activate
+
+cekit  build --overrides-file quarkus-native-binary-s2i-overrides.yaml ${BUILD_ENGINE} --tag="${NAME}"
