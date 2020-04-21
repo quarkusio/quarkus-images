c4d0a5f3c8d9f3304b1c70b32f0e56dc6cac3461
Clement Escoffier <clement.escoffier@gmail.com>
2020-04-21 09:28:57 +0200

    Do not squash the native s2i

--- .github/build-s2i-native-images.sh
+++ .github/build-s2i-native-images.sh
@@ -9,5 +9,5 @@ TAG_JAVA11=${NAME}:${IMAGE_VERSION}-java11
 virtualenv --python=python3 ~/cekit
 source ~/cekit/bin/activate
 
-cekit  build --overrides-file quarkus-native-s2i-overrides-java8.yaml ${BUILD_ENGINE} --tag="${TAG_JAVA8}"
-cekit  build --overrides-file quarkus-native-s2i-overrides-java11.yaml ${BUILD_ENGINE} --tag="${TAG_JAVA11}"
+cekit  build --overrides-file quarkus-native-s2i-overrides-java8.yaml ${BUILD_ENGINE} --tag="${TAG_JAVA8}" --no-squash
+cekit  build --overrides-file quarkus-native-s2i-overrides-java11.yaml ${BUILD_ENGINE} --tag="${TAG_JAVA11}" --no-squash
