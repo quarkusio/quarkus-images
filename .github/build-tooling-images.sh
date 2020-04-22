84d2fd0bf27aec2eff86bb063a48df805c19b5f4
Clement Escoffier <clement.escoffier@gmail.com>
2020-04-22 13:50:18 +0200

    Reclaim disk space before build

--- .github/build-tooling-images.sh
+++ .github/build-tooling-images.sh
@@ -9,7 +9,9 @@ TAG_JAVA11=${NAME}:${IMAGE_VERSION}-java11
 virtualenv --python=python3 ~/cekit
 source ~/cekit/bin/activate
 
-cekit  build --overrides-file quarkus-maven-overrides-java8.yaml ${BUILD_ENGINE} --tag="${TAG_JAVA8}" --no-squash
-cekit  build --overrides-file quarkus-maven-overrides-java11.yaml ${BUILD_ENGINE} --tag="${TAG_JAVA11}" --no-squash
+
+
+cekit  build --overrides-file quarkus-maven-overrides-java8.yaml ${BUILD_ENGINE} --tag="${TAG_JAVA8}" 
+cekit  build --overrides-file quarkus-maven-overrides-java11.yaml ${BUILD_ENGINE} --tag="${TAG_JAVA11}"
 
 docker image prune -f
