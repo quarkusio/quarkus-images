3456624a63371b7e4c6f9a77a30032c6e1b68c1c
Foivos Zakkak <fzakkak@redhat.com>
2020-08-18 23:16:37 +0300

    Create major.minor tag for the latest major.minor.x.x mandrel version

--- .github/build-mandrel-images.sh
+++ .github/build-mandrel-images.sh
@@ -17,5 +17,12 @@ do
         ${BUILD_ENGINE} --tag="${PREFIX_NAME}:${version}"
 done
 
+# Get all the final versions, reverse sort them, and keep only the first for each major.minor (e.g. 20.1)
+LATEST_FINAL_VERSIONS=$(sed 's/ /\n/g' <<< ${VERSIONS[@]} | grep Final | sort -r | sort -k1,1 -k2,2 -k5,5 -t'.' --unique)
+for latest in ${LATEST_FINAL_VERSIONS[@]}
+do
+    ${BUILD_ENGINE} tag ${PREFIX_NAME}:${latest} ${PREFIX_NAME}:${latest/.[^.].[^.].Final/}
+done
+
 ${BUILD_ENGINE} image prune -f
 ${BUILD_ENGINE} images
