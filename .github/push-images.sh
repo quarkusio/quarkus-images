c18959c1dbb0fae9de7df644ff73cf710691e719
Clement Escoffier <clement.escoffier@gmail.com>
2020-10-27 08:51:40 +0100

    Do not push images generated during the tests

--- .github/push-images.sh
+++ .github/push-images.sh
@@ -4,7 +4,8 @@ set -e
 docker login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
 
 # Retrieve the produces images and push them
-for IMG in $(docker images | grep quay.io/quarkus/ | awk '{print $1":"$2}') 
+# We need to ignore images prefeixed with integ- which are created during the tests.
+for IMG in $(docker images | grep quay.io/quarkus/ | grep -v "integ-quay" |awk '{print $1":"$2}') 
 do 
     echo "Pushing ${IMG}"
     docker push ${IMG}
