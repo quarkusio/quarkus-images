06e4412b56141240cae0c729d936e4be6cd40825
Galder Zamarreño <galder@zamarreno.com>
2022-05-10 16:23:21 +0200

    Add multi-arch native image and Mandrel image push steps

--- .github/push-images.sh
+++ .github/push-images.sh
@@ -10,3 +10,39 @@ do
     echo "Pushing ${IMG}"
     docker push ${IMG}
 done
+
+manifest_push () {
+    local image=$1
+    local tag=$2
+
+    docker manifest create quay.io/quarkus/${image}:${tag} \
+        --amend quay.io/quarkus/${image}:${tag}-amd64 \
+        --amend quay.io/quarkus/${image}:${tag}-arm64
+    docker manifest push quay.io/quarkus/${image}:${tag}
+}
+
+# Manually combine manifests for multi-arch native builder images
+if [[ $(docker images | grep quay.io/quarkus/ | grep "ubi-quarkus-native-image") ]]; then
+    manifest_push "ubi-quarkus-native-image" "22.0.0-java17"
+    manifest_push "ubi-quarkus-native-image" "22.0.0-java11"
+    manifest_push "ubi-quarkus-native-image" "22.0-java17"
+    manifest_push "ubi-quarkus-native-image" "22.0-java11"
+
+    manifest_push "ubi-quarkus-native-image" "22.1.0-java17"
+    manifest_push "ubi-quarkus-native-image" "22.1.0-java11"
+    manifest_push "ubi-quarkus-native-image" "22.1-java17"
+    manifest_push "ubi-quarkus-native-image" "22.1-java11"
+fi
+
+# Manually combine manifests for multi-arch mandrel images
+if [[ $(docker images | grep quay.io/quarkus/ | grep "ubi-quarkus-mandrel") ]]; then
+    manifest_push "ubi-quarkus-mandrel" "22.0.0.2.Final-java17"
+    manifest_push "ubi-quarkus-mandrel" "22.0.0.2.Final-java11"
+    manifest_push "ubi-quarkus-mandrel" "22.0-java17"
+    manifest_push "ubi-quarkus-mandrel" "22.0-java11"
+
+    manifest_push "ubi-quarkus-mandrel" "22.1.0.0.Final-java17"
+    manifest_push "ubi-quarkus-mandrel" "22.1.0.0.Final-java11"
+    manifest_push "ubi-quarkus-mandrel" "22.1-java17"
+    manifest_push "ubi-quarkus-mandrel" "22.1-java11"
+fi
