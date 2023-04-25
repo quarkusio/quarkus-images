63c301c08beb3d6bb3f8e5caed540de77c0d0a7b
Clement Escoffier <clement@apache.org>
2023-04-25 20:41:32 +0200

    Create a fix version for the ubi version

--- quarkus-micro-base-image/src/main/java/io/quarkus/images/Build.java
+++ quarkus-micro-base-image/src/main/java/io/quarkus/images/Build.java
@@ -7,6 +7,7 @@ package io.quarkus.images;
 import picocli.CommandLine;
 
 import java.io.File;
+import java.util.Optional;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "build")
@@ -24,6 +25,10 @@ public class Build implements Callable<Integer> {
     @CommandLine.Option(names = {
             "--dockerfile-dir" }, description = "The location where the docker file should be created", defaultValue = "target/docker")
     private File dockerFileDir;
+
+    @CommandLine.Option(names = { "--alias" }, description = "An optional alias for the output image (ignored)")
+    @Deprecated
+    private Optional<String> alias;
 
     @CommandLine.Option(names = "--dry-run", description = "Just generate the docker file and skip the container build")
     private boolean dryRun;
