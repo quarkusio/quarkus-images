11eeb25d731453f0b99e3b2a7f759b97a076cd1d
Guillaume Smet <guillaume.smet@gmail.com>
2026-06-26 14:21:18 +0200

    Update UBI 9 to 9.8 and UBI 10 to 10.2

--- jdock/src/test/java/io/quarkus/images/utils/RpmsTest.java
+++ jdock/src/test/java/io/quarkus/images/utils/RpmsTest.java
@@ -22,28 +22,28 @@ class RpmsTest {
 
     @Test
     void testZlibDevelUbi9Minimal() {
-        String base = "registry.access.redhat.com/ubi9/ubi-minimal:9.7";
+        String base = "registry.access.redhat.com/ubi9/ubi-minimal:9.8";
         assertThat(Rpms.zlibDevel(base))
                 .isEqualTo("zlib-devel");
     }
 
     @Test
     void testZlibDevelUbi9Micro() {
-        String base = "registry.access.redhat.com/ubi9-micro:9.7";
+        String base = "registry.access.redhat.com/ubi9-micro:9.8";
         assertThat(Rpms.zlibDevel(base))
                 .isEqualTo("zlib-devel");
     }
 
     @Test
     void testZlibDevelUbi10Minimal() {
-        String base = "registry.access.redhat.com/ubi10/ubi-minimal:10.1";
+        String base = "registry.access.redhat.com/ubi10/ubi-minimal:10.2";
         assertThat(Rpms.zlibDevel(base))
                 .isEqualTo("zlib-ng-compat-devel");
     }
 
     @Test
     void testZlibDevelUbi10Micro() {
-        String base = "registry.access.redhat.com/ubi10-micro:10.1";
+        String base = "registry.access.redhat.com/ubi10-micro:10.2";
         assertThat(Rpms.zlibDevel(base))
                 .isEqualTo("zlib-ng-compat-devel");
     }
